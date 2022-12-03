afcda3616e993f0d24b0a1d5a6b512cafd36aea2
Beserrovsky <felipebeserra25@gmail.com>
2022-12-03 16:02:07 -0300

    Website created and rasp-cli stuff

---
+++ rasp-client/node_modules/@picovoice/rhino-node/test/index.test.ts
@@ -0,0 +1,210 @@
+//
+// Copyright 2020-2022 Picovoice Inc.
+//
+// You may not use this file except in compliance with the license. A copy of the license is located in the "LICENSE"
+// file accompanying this source.
+//
+// Unless required by applicable law or agreed to in writing, software distributed under the License is distributed on
+// an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the License for the
+// specific language governing permissions and limitations under the License.
+//
+"use strict";
+
+import Rhino, {RhinoInference} from "../src/rhino";
+
+import * as fs from "fs";
+import {checkWaveFile, getInt16Frames} from "../src";
+import {WaveFile} from "wavefile";
+
+import {RhinoInvalidArgumentError} from "../src/errors";
+import {getAudioFileByLanguage, getContextPathsByLanguage, getModelPathByLanguage} from "./test_utils"
+
+const WITHIN_CONTEXT_PARAMETERS: [string, string, string, Record<string, string>][] = [
+    ['en', 'coffee_maker', 'orderBeverage',
+        {'beverage': 'americano', 'numberOfShots': 'double shot', 'size': 'medium'}],
+    ['es', 'iluminación_inteligente', 'changeColor', {'location': 'habitación', 'color': 'rosado'}],
+    ['de', 'beleuchtung', 'changeState', {'state': 'aus'}],
+    ['fr', 'éclairage_intelligent', 'changeColor', {'color': 'violet'}],
+    ['it', 'illuminazione', 'spegnereLuce', {'luogo': 'bagno'}],
+    ['ja', 'sumāto_shōmei', '色変更', {'色': '青'}],
+    ['ko', 'seumateu_jomyeong', 'changeColor', {'color': '파란색'}],
+    ['pt', 'luz_inteligente', 'ligueLuz', {'lugar': 'cozinha'}],
+]
+
+const OUT_OF_CONTEXT_PARAMETERS = [
+    ['en', 'coffee_maker'],
+    ['es', 'iluminación_inteligente'],
+    ['de', 'beleuchtung'],
+    ['fr', 'éclairage_intelligent'],
+    ['it', 'illuminazione'],
+    ['ja', 'sumāto_shōmei'],
+    ['ko', 'seumateu_jomyeong'],
+    ['pt', 'luz_inteligente'],
+]
+
+const ACCESS_KEY = process.argv.filter((x) => x.startsWith('--access_key='))[0]?.split('--access_key=')[1] ?? "";
+const PERFORMANCE_THRESHOLD_SEC = Number(process.argv.filter((x) => x.startsWith('--performance_threshold_sec='))[0]?.split('--performance_threshold_sec=')[1] ?? 0);
+const describe_if = (condition: boolean) => condition ? describe : describe.skip;
+
+
+function testRhinoDetection(
+    language: string,
+    context: string,
+    is_within_context: boolean,
+    groundTruth: RhinoInference | null = null): void {
+
+    const contextPath = getContextPathsByLanguage("../../../", language, context);
+
+    const modelPath = getModelPathByLanguage("../../../", language);
+    const engineInstance = new Rhino(
+        ACCESS_KEY,
+        contextPath,
+        0.5,
+        1.0,
+        true,
+        modelPath,
+    );
+
+    const waveFilePath = getAudioFileByLanguage("../../../", language, is_within_context);
+    const waveBuffer = fs.readFileSync(waveFilePath);
+    const waveAudioFile = new WaveFile(waveBuffer);
+
+    if (!checkWaveFile(waveAudioFile, engineInstance.sampleRate)) {
+        fail(
+            "Audio file did not meet requirements. Wave file must be 16KHz, 16-bit, linear PCM (mono)."
+        );
+    }
+
+    const frames = getInt16Frames(waveAudioFile, engineInstance.frameLength);
+
+    let isFinalized = false;
+    for (let i = 0; i < frames.length; i++) {
+        const frame = frames[i];
+        isFinalized = engineInstance.process(frame);
+
+        if (isFinalized) {
+            if (groundTruth !== null) {
+                expect(engineInstance.getInference()).toEqual(groundTruth);
+            } else {
+                expect(engineInstance.getInference().isUnderstood).toBe(false);
+            }
+        }
+    }
+
+}
+
+describe("intent detection", () => {
+        it.each(WITHIN_CONTEXT_PARAMETERS)(
+            'successful inference for %p with %p', (language, context, intent, slots) => {
+                const inference: RhinoInference = {
+                    isUnderstood: true,
+                    intent: intent,
+                    slots: slots,
+                };
+                testRhinoDetection(language, context, true, inference);
+            });
+
+        it.each(OUT_OF_CONTEXT_PARAMETERS)(
+            'out-of-context phrase for %p with %p', (language, context) => {
+                testRhinoDetection(language, context, false);
+            });
+
+    }
+);
+
+describe("basic parameter validation", () => {
+    test("invalid sensitivity range", () => {
+        expect(() => {
+            new Rhino(
+                ACCESS_KEY,
+                getContextPathsByLanguage("../../../", 'en', 'coffee_maker'),
+                2.99);
+        }).toThrow(RangeError);
+    });
+
+    test("invalid sensitivity type", () => {
+        expect(() => {
+            const rhinoEngine = new Rhino(
+                ACCESS_KEY,
+                getContextPathsByLanguage("../../../", 'en', 'coffee_maker'),
+                // @ts-expect-error
+                "invalid_sensitivity"
+            );
+        }).toThrow(RangeError);
+    });
+});
+
+describe("frame validation", () => {
+    test("accepts non Int16Array if array is valid", () => {
+        const rhinoEngine = new Rhino(
+            ACCESS_KEY,
+            getContextPathsByLanguage("../../../", 'en', 'coffee_maker'));
+        const emptyArray = Array.apply(null, Array(rhinoEngine.frameLength)).map((x, i) => i)
+        // @ts-expect-error
+        rhinoEngine.process(emptyArray);
+        rhinoEngine.release();
+    });
+
+    test("mismatched frameLength throws error", () => {
+        const rhinoEngine = new Rhino(
+            ACCESS_KEY,
+            getContextPathsByLanguage("../../../", 'en', 'coffee_maker'));
+        expect(() => {
+            // @ts-expect-error
+            rhinoEngine.process([1, 2, 3]);
+        }).toThrow(RhinoInvalidArgumentError);
+        rhinoEngine.release();
+    });
+
+    test("null/undefined frames throws error", () => {
+        const rhinoEngine = new Rhino(
+            ACCESS_KEY,
+            getContextPathsByLanguage("../../../", 'en', 'coffee_maker'),
+        );
+        expect(() => {
+            // @ts-expect-error
+            rhinoEngine.process(null);
+        }).toThrow(RhinoInvalidArgumentError);
+        expect(() => {
+            // @ts-expect-error
+            rhinoEngine.process(undefined);
+        }).toThrow(RhinoInvalidArgumentError);
+        rhinoEngine.release();
+    });
+
+    test("passing floating point frame values throws RhinoInvalidArgumentError", () => {
+        const rhinoEngine = new Rhino(
+            ACCESS_KEY,
+            getContextPathsByLanguage("../../../", 'en', 'coffee_maker'),
+        );
+        const floatFrames = Array.from({length: rhinoEngine.frameLength}).map(
+            (x) => 3.1415
+        );
+        expect(() => {
+            // @ts-expect-error
+            rhinoEngine.process(floatFrames);
+        }).toThrow(RhinoInvalidArgumentError);
+        rhinoEngine.release();
+    });
+});
+
+describe("getContextInfo", () => {
+    test("coffee maker expressions and slots are returned", () => {
+        const rhinoEngine = new Rhino(
+            ACCESS_KEY,
+            getContextPathsByLanguage("../../../", 'en', 'coffee_maker'),
+        );
+
+        const contextInfo = rhinoEngine.getContextInfo();
+
+        expect(contextInfo).toMatch(
+            /(\[brew, can I get, can I have, I want, get me, give me, I'd like, make me, may I have)/i
+        );
+        expect(contextInfo).toMatch(/(- "triple shot")/i);
+        expect(contextInfo).not.toMatch(
+            /(the third one burned down, fell over, and sank into the swamp)/i
+        );
+
+        rhinoEngine.release();
+    });
+});
